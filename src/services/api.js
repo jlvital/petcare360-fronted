f041bf33f599272cd80825ee6e3c52f65cb82698
@jlvital <jl.vital.mascort@gmail.com>
2025-04-28 01:12:53 +0200

    FIX CONFIGURATION v.2.2 >> Entorno para Vite + Vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-// Puedes mover esta URL a un archivo de configuración (.env) para despliegue
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
